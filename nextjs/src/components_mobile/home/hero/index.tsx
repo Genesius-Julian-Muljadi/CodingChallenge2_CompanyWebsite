88f106533f3b18f02de46273dbb1c0e49d4b2360
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2024-10-10 19:00:43 +0700

    .

--- nextjs/src/components_mobile/home/hero/index.tsx
+++ nextjs/src/components_mobile/home/hero/index.tsx
@@ -4,7 +4,7 @@ export default function HeroMobile() {
     return (
         <div className="mt-2">
             <div className="flex flex-col gap-2">
-                <div className="text-xl text-center" id="homesloganmobile">
+                <div className="text-xl text-center text-nowrap" id="homesloganmobile">
                     FOR THOSE WHO DARE TO SHINE BRIGHT
                 </div>
                 <div className="rounded-3xl mx-auto" id="homeherocontainermobile"></div>
