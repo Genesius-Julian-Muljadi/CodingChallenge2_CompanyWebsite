21702251133be01b65fa50a26660ef31f7e8ae83
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2024-10-10 18:53:12 +0700

    .

--- nextjs/src/components_mobile/home/companyoverview/index.tsx
+++ nextjs/src/components_mobile/home/companyoverview/index.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 export default function CompanyOverviewMobile() {
     return (
         <div className="flex mx-3" id="cocontainermobile">
-            <div className="text-wrap text-left mx-2 font-bold" id="cotextmobile">
+            <div className="text-wrap text-left font-bold" id="cotextmobile">
                 Idolla trades quality gold and diamond jewelry at competitive prices. Whether you're buying or selling in-store or at-home, you'll be sure to get the best deal on the market!
             </div>
         </div>
