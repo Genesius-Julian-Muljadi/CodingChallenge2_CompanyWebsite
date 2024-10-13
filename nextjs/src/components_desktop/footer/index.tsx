a344002f74377f9cb2b956e9f40fb9bf56ae2e77
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2024-10-13 13:46:56 +0700

    .

--- nextjs/src/components_desktop/footer/index.tsx
+++ nextjs/src/components_desktop/footer/index.tsx
@@ -7,9 +7,9 @@ export default function Footer() {
     return (
         <div className="grid grid-cols-1 grid-rows-1">
             <div className="col-start-1 row-start-1 flex sm:hidden"><FooterMobile /></div>
-            <div className="col-start-1 row-start-1 hidden sm:grid sm:grid-cols-9 sm:gap-10 h-[14rem] bg-gray-600"
+            <div className="col-start-1 row-start-1 hidden sm:grid sm:grid-cols-2 sm:grid-rows-1 h-[14rem] bg-gray-600"
                 id="footerdiv">
-                <div className="col-start-6 col-end-10 row-start-1 my-auto">
+                <div className="col-start-2 row-start-1 my-auto pl-16 border-l border-white">
                     <ContactUs />
                     {/* <IdollaGoogleMaps /> */}
                 </div>
