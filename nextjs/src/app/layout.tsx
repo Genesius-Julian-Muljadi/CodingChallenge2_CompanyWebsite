014fc6bac236ec0bf5f4439c9fbadd9c0e2394eb
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2024-10-15 17:00:44 +0700

    .

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -11,6 +11,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content="For those who dare to shine bright: Idolla trades quality gold and diamond jewelry at competitive prices. Whether you're buying or selling in-store or at-home, you'll be sure to get the best deal on the market!" />
+      </head>
       <body>
         {/* <Provider store={store}> */}
           {/* <NavBar /> */}
