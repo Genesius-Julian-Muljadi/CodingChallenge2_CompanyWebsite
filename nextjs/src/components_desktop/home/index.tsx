b0d9791f397638ac40ba3b8cd691724a24cdad53
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2024-12-12 16:43:14 +0700

    portfolio description

--- nextjs/src/components_desktop/home/index.tsx
+++ nextjs/src/components_desktop/home/index.tsx
@@ -9,15 +9,18 @@ import Testimonials from "./testimonials";
 
 export default function HomeView() {
     return (
-        <div className="grid grid-cols-1 grid-rows-1" id="background">
-            <div className="col-start-1 row-start-1 flex sm:hidden"><HomeMobile /></div>
-            <div className="col-start-1 row-start-1 hidden sm:grid mt-6">
-                <div className="grid grid-cols-4 grid-rows-1 gap-4 mx-32">
-                    <div className="col-start-1 row-start-1 col-end-3"><HeroSection /></div>
-                    <div className="col-start-3 row-start-1 col-end-5"><CompanyOverview /></div>
+        <div className="flex flex-col gap-4">
+            <p>My first project. Front-end-only using Next.js, Tailwind CSS, & Contentful for the catalogue</p>
+            <div className="grid grid-cols-1 grid-rows-1" id="background">
+                <div className="col-start-1 row-start-1 flex sm:hidden"><HomeMobile /></div>
+                <div className="col-start-1 row-start-1 hidden sm:grid mt-6">
+                    <div className="grid grid-cols-4 grid-rows-1 gap-4 mx-32">
+                        <div className="col-start-1 row-start-1 col-end-3"><HeroSection /></div>
+                        <div className="col-start-3 row-start-1 col-end-5"><CompanyOverview /></div>
+                    </div>
+                    <div><ProductsServices /></div>
+                    <div className="w-[80vw] mx-auto mb-6"><Testimonials /></div>
                 </div>
-                <div><ProductsServices /></div>
-                <div className="w-[80vw] mx-auto mb-6"><Testimonials /></div>
             </div>
         </div>
     );
